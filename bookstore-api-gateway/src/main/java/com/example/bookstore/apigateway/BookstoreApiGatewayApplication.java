0a2957c77534bb6c98d2106e66ee48403177af31
kenduraghav <kenduraghav@gmail.com>
2026-06-02 11:00:35 +0530

    Issue #16: Implement API Gateway

---
+++ bookstore-api-gateway/src/main/java/com/example/bookstore/apigateway/BookstoreApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.example.bookstore.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApiGatewayApplication.class, args);
+    }
+}
