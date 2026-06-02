3963bc777c75004fd0b36c702cc35a576b1bbb83
kenduraghav <kenduraghav@gmail.com>
2026-06-02 12:26:43 +0530

    #17 Bookstore Webapp Skeleton setup

---
+++ bookstore-webapp/src/main/java/com/example/bookstore/webapp/web/controller/ProductController.java
@@ -0,0 +1,19 @@
+package com.example.bookstore.webapp.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class ProductController {
+
+	
+	@GetMapping
+	String index() {
+		return "redirect:/products";
+	}
+	
+	@GetMapping("/products")
+	String products() {
+		return "products";
+	}
+}
